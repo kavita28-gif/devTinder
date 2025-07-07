27b1e3afd158cfcd07bbda5cbd888b4229674c7b
Kavita <a@Kavitas-Air.bbrouter>
2025-07-07 14:37:02 +0530

    Exploring routes get, post, put , delete

--- src/app.js
+++ src/app.js
@@ -2,18 +2,43 @@ const express = require('express');
 
 const app = express();
 
-// app.use((req, res) => {     // This is a request handler for all routes
-//     res.send("This is request handler");
+// app.use("/hello/2", (req, res) => {     // This is a request handler for all routes
+//     res.send("Hello2 hello2 hello2");
+// });
+
+// app.use("/hello", (req, res) => {     // This is a request handler for all routes
+//     res.send("Hello hello hello");
 // });
 
 
 
+app.get("/user", (req, res) => {
+    res.send({"firstName": "Kavita", "lastName": "Kinagi"});
+});
+
+app.post("/user", (req, res) => {
+    res.send("Successfully insered data to the database");
+});
+
+app.put("/user", (req, res) => {
+    res.send("Successfully updated user data!");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Successfully deleted user data");
+});
+
 app.use("/test", (req, res) => {    // This is a request handler for /test
     res.send("This is a test route");
-})
+});
 
 app.use("/", (req, res) => {      // This is a request handler for /
     res.send("this is a / route");
+});
+
+
+app.use((req, res) => {
+    res.send("This is a base route, other routes won't work")
 })
 
 app.listen(4000, ()=> {
