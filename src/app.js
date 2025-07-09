76ba513b54ed93d0f1625a523d2d81e28f20d388
Kavita <a@Kavitas-Air.bbrouter>
2025-07-09 14:10:25 +0530

    Schema Validations

--- src/app.js
+++ src/app.js
@@ -3,24 +3,80 @@ const express = require('express');
 const app = express();
 
 const connectDB = require('./config/database');
-const User = require('./models/user')
+const User = require('./models/user');
+
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
+    // creating a new instance of user model
+    console.log(req.body);
 
-    const user = new User({
-        firstName: "Kavita",
-        lastName: "Kinagi",
-        emailId: "kavita@kinagi.com",
-        password: "kavita123"
-    });
+    const user = new User(req.body);
 
     try {
         await user.save();
-
         res.status(200).send("User added successfully!!");
     } catch (err) {
-        res.status(400).send("Something went wrong!!" + err.message);
+        res.status(400).send("Error saving user" + err.message);
     }
+});
+
+app.get("/user", async (req, res) => {
+
+    const emailId = req.body.emailId;
+    // console.log(email);
+
+    try {
+        const user = await User.findOne({ emailId });
+        if(!user) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(user);
+        }
+        
+    } catch (err) {
+        res.status(500).send("Something went wrong");
+    }
+})
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find();
+        if(users.length === 0) {
+            res.status(404).send("Users not found");
+        }
+        res.send(users);
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+
+})
+
+app.delete("/user", async (req, res)=> {
+    try {
+        const userId = req.body.userId;
+        const user = await User.findByIdAndDelete(userId);
+        res.send("Successfully deleted user");
+
+    } catch(err) {
+        res.status(500).send("Something went erong")
+    }
+})
+
+
+app.patch("/user", async (req, res) => {
+    const userId = req.body._id;
+
+    try {
+        const user = await User.findOneAndUpdate({_id: userId}, req.body, {
+            returnDocument: 'after',
+            runValidators: true
+        }); // bydefault it returns before update data
+        res.send(user);
+    } catch (err) {
+        res.status(400).send("Error updating user" + err.message);
+    }
+
 })
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -5,22 +5,56 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
     firstName: {
         type : String,
+        required: true,
+        minLength: 4,
+        maxLength: 100
     },
     lastName: {
         type : String,
+        minLength: 4,
+        maxLength: 100,
     },
     emailId: {
         type:  String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true
     },
     password: {
         type: String,
+        required: true,
+        minLength: 8,
+        maxLength: 100,
     },
     age: {
         type: Number,
+        min: 18,
     },
     gender: {
-        type: String
+        type: String,
+        validate(value) {
+            if(!["male", "female", "other"].includes(value)) {
+                throw new Error("Gender data is not valid");
+            }
+        }
     },
+    phptoUrl: {
+        type: String,
+        default: "https://ongcvidesh.com/company/board-of-directors/dummy-image/",
+        
+    },
+    about: {
+        type: String,
+        default: "This is a default description of the user",
+        maxLength: 500,
+    },
+    skills: {
+        type: [String],
+    },
+},
+{
+    timestamps: true
 })
 
 const User = mongoose.model("User", userSchema);
